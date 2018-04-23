39b3e756aa08e9baf6de2210dd07fdc15413f890
tykoe <38664100+tykoe@users.noreply.github.com>
2018-04-24 01:37:04 +1000

    Update Unifi_Controller.sh

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo “deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti” | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv –keyserver keyserver.ubuntu.com –recv 06E85760C0A52C50
 sudo apt-get update
 sudo apt-get install unifi -y
 echo "echo \"\"" >> /etc/profile
